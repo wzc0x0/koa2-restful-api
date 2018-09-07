bca357f920a892d13691ab275211c43baa60f739
wzc0x0 <wangzichen@peilian.com>
2018-09-07 20:40:08 +0800

    save code

--- src/app.js
+++ src/app.js
@@ -7,6 +7,16 @@ const router = new Router()
 
 app.use(bodyParser())
 
+app.use((ctx, next) => {
+    if (ctx.request.header.host.split(':')[0] === 'localhost' || ctx.request.header.host.split(':')[0] === '127.0.0.1') {
+        ctx.set('Access-Control-Allow-Origin', '*')
+    }
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+    ctx.set('Access-Control-Allow-Credentials', true) // 允许带上 cookie
+    return next()
+})
+
 router
     .get('/', (ctx, next) => {
         ctx.body = '<h1>hello get</h1>'
@@ -28,6 +38,16 @@ router
     })
     .put('/user', (ctx, next) => {
 
+    })
+    .get('/get-cookie', (ctx, next) => {
+        ctx.cookies.set('sid', 'hello world', {
+            domain: 'localhost',
+            path: '/',
+            maxAge: 3600,
+            expires: new Date(),
+            httpOnly: false
+        })
+        ctx.body = 'cookie is ok'
     })
 
 app
